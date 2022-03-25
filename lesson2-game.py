b8f2c37a79f1f8996511c4d2a22261478026a79b
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-25 08:59:09 +0200

    Update lesson2-game.py

--- lesson2-game.py
+++ lesson2-game.py
@@ -21,20 +21,30 @@ def move(step, current_position_x, current_position_y):
     print("Current step is: ", step)
     print("Current current_position_y = ", current_position_y)
     print("Current current_position_x = ", current_position_x)
+
     can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 0
     can_move_bottom = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 0
 
+    right_is_finish = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 24
+    bottom_is_finish = current_position_x <= 2 and map[current_position_y+1][current_position_x] == 24
+
+    if right_is_finish:
+        print("Found finish on right")
+        return False 
+
+    if bottom_is_finish:
+        print("Found finish on bottom")
+        return False 
+
     if can_move_right:
         print("Should move right")
-        current_position_x = current_position_x + 1
+        return [current_position_x + 1, current_position_y]
     if can_move_bottom:
         print("Should move down")
-        current_position_y = current_position_y + 1
-    
-    return [current_position_x, current_position_y]
+        return [current_position_x, current_position_y + 1]
 
-new_position = move(1, start_position_x, start_position_y)
-new_position = move(2, new_position[0], new_position[1])
-new_position = move(3, new_position[0], new_position[1])
-
-# this is change
+current_move_number = 1    
+new_position = move(current_move_number, start_position_x, start_position_y)
+while new_position:
+    current_move_number = current_move_number + 1
+    new_position = move(current_move_number, new_position[0], new_position[1])
